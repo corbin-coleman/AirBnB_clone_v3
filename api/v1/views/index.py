0054195fa8c3c1b94e52257b35259135eaf69e03
Corbin <corbincol@hotmail.com>
2017-04-25 21:14:04 +0000

    Task 5 - Retrieve number of objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,19 @@ index
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status')
 def status_ok():
-    return jsonify(status="OK")
+    return jsonify({'status': "OK"})
+
+@app_views.route('/api/v1/stats')
+def stats():
+    objects = {}
+    objects['amenities'] = storage.count('Amenity')
+    objects['cities'] = storage.count('City')
+    objects['places'] = storage.count('Place')
+    objects['reviews'] = storage.count('Review')
+    objects['states'] = storage.count('State')
+    objects['users'] = storage.count('User')
+
