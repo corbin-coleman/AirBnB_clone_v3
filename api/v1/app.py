486e4e75386ae78199ddad5da39487909db05e79
Corbin <corbincol@hotmail.com>
2017-04-25 23:02:09 +0000

    Task 6 - 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,15 +3,21 @@
 API
 """
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def teardown():
+def teardown(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'error': "Not found"})
 
 if __name__ == '__main__':
     host = getenv("HBNB_API_HOST")
