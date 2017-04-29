7fead7677081321395a5539153b50794d5576179
Corbin <corbincol@hotmail.com>
2017-04-29 04:41:24 +0000

    Task 14 - Setup flask CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,12 @@ API
 """
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+CORS(app, origin='0.0.0.0')
 
 @app.teardown_appcontext
 def teardown(exception):
